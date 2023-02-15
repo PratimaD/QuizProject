2c002a14f0340c9b62788fbed09002629eef934a
Pratima <31152279+PratimaD@users.noreply.github.com>
2023-02-15 14:37:42 +0530

    Add files via upload

---
+++ Quiz_Project/src/main/resources/data.sql
@@ -0,0 +1,49 @@
+insert into questions(ques_id, title, optionA, optionB, ans, chose)
+values(
+	1, 
+	'Are you proud to be a member of your team?',  
+	'Yes',
+	'No',
+	1,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, ans, chose)
+values(
+	2, 
+	'Are you able to work productively in your remotework environment?',  
+	'Yes',
+	'No',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, ans, chose)
+values(
+	3, 
+	'Is there a clear understanding of the organizations strategic objectives?',  
+	'Yes',
+	'No',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, ans, chose)
+values(
+	4, 
+	'Do you feel you are rewarded for your dedication and commitment to the work?',  
+	'Yes',
+	'No',
+	1,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, ans, chose)
+values(
+	5, 
+	'Are you satisfied with your job overall?',  
+	'Yes',
+	'No',
+	1,
+	-1)
+;
